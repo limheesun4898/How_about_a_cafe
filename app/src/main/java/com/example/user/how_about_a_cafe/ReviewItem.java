7be1ef87e00b5d254ac7a9bf37a53b2b0225f496
hyunjin <dksguswls121@naver.com>
2018-10-02 13:10:25 +0900

    리뷰 2차

--- app/src/main/java/com/example/user/how_about_a_cafe/ReviewItem.java
+++ app/src/main/java/com/example/user/how_about_a_cafe/ReviewItem.java
@@ -3,14 +3,24 @@ package com.example.user.how_about_a_cafe;
 public class ReviewItem {
     private String review;
     private String rating;
+    private String url;
+    private boolean isimage;
 
     ReviewItem() {
 
     }
 
-    ReviewItem(String review, String rating) {
+    ReviewItem(String review, String rating, boolean isimage) {
         this.review = review;
         this.rating = rating;
+        this.isimage = isimage;
+    }
+
+    ReviewItem(String review, String rating, String url, boolean isimage) {
+        this.review = review;
+        this.rating = rating;
+        this.url = url;
+        this.isimage = isimage;
     }
 
     public String getReview() {
@@ -20,4 +30,8 @@ public class ReviewItem {
     public String getRating() {
         return this.rating;
     }
+
+    public String getUrl() {return this.url;}
+
+    public boolean isIsimage() {return this.isimage;}
 }
--- app/src/main/java/com/example/user/how_about_a_cafe/WriteReview.java
+++ app/src/main/java/com/example/user/how_about_a_cafe/WriteReview.java
@@ -2,8 +2,12 @@ package com.example.user.how_about_a_cafe;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,16 +18,24 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -33,6 +45,7 @@ import java.util.List;
 //리뷰는 무조건 별점이 있어야하고 별점 없이는 리뷰를 작성할 수 없도록 만듬. 그래서 별점이 없는 리뷰는 없음.
 public class WriteReview extends AppCompatActivity {
     public static DatabaseReference firebaseDatabase = FirebaseDatabase.getInstance().getReference();
+    private StorageReference mStorge = FirebaseStorage.getInstance().getReference();
     private EditText editReview;
     private TextView textNum;
     private Spinner spinner;
@@ -42,6 +55,12 @@ public class WriteReview extends AppCompatActivity {
     private String cafe_name;
     private List<String> data;
     private String menu;
+    private TextView photo_set;
+    private ImageView photo_select;
+    private int PERMISSIONS_READ_STORAGE = 0;
+    static Task<Uri> downloadUrl;
+    static boolean isimage = false;
+    final int GALLERY_INTENT = 100;
 
     long now = System.currentTimeMillis();
     Date date = new Date(now);
@@ -60,6 +79,8 @@ public class WriteReview extends AppCompatActivity {
         editReview = (EditText) findViewById(R.id.write_review_edit);
         spinner = (Spinner) findViewById(R.id.spinner);
         ratingBar = (RatingBar) findViewById(R.id.ratingBar1);
+        photo_select = (ImageView) findViewById(R.id.write_review_photo_select);
+        photo_set = (TextView) findViewById(R.id.write_review_set_photo);
 
         Toolbar mytoolbar = findViewById(R.id.write_review_toolbar);
         setSupportActionBar(mytoolbar);
@@ -71,6 +92,13 @@ public class WriteReview extends AppCompatActivity {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
                 rating = v;     //v가 별 개수
+            }
+        });
+
+        photo_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                permission();
             }
         });
 
@@ -200,6 +228,90 @@ public class WriteReview extends AppCompatActivity {
             }
         });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode != RESULT_OK)
+            return;
+
+        switch (requestCode) {
+            case GALLERY_INTENT:
+                setList_image(data);
+                break;
+
+            default:
+                break;
+
+        }
+    }
+    public void setList_image(Intent data) {
+        Uri file = data.getData();
+        final StorageReference filePath = mStorge.child("images/" + formatDate);
+
+        filePath.putFile(file).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                downloadUrl = filePath.getDownloadUrl();
+                isimage = true;
+                photo_set.setText("이미지가 등록되었습니다");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(WriteReview.this, "예외 발생" + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 0:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("Test", "Permission ok");
+                    pto();
+                } else {
+                    Log.e("Test", "Permission deny");
+                    AlertDialog.Builder alert_confirm = new AlertDialog.Builder(this);
+                    alert_confirm.setTitle("사진에 접근하기 위해 해당 권한이 필요합니다.").setMessage("[설정] > [권한]에서 해당 권한을 활성화 시켜주세요.")
+                            .setCancelable(false).setPositiveButton("설정",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                            .setData(Uri.parse("package:" + getPackageName()));
+                                    startActivity(intent);
+                                }
+                            }).setNegativeButton("닫기",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.cancel();
+                                }
+                            });
+                    AlertDialog alert = alert_confirm.create();
+                    alert.show();
+                }
+                break;
+        }
+    }
+
+    public void pto() {
+        final int GALLERY_INTENT = 100;
+
+        Intent i = new Intent(Intent.ACTION_PICK);
+        i.setType("image/*");
+        startActivityForResult(i, GALLERY_INTENT);
+    }
+    public void permission() {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                PERMISSIONS_READ_STORAGE);
+
+    }
 
     public void alt() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -224,10 +336,20 @@ public class WriteReview extends AppCompatActivity {
                 else {
                     Toast.makeText(WriteReview.this, "리뷰가 작성되었습니다", Toast.LENGTH_SHORT).show();
 
-                    ReviewItem a = new ReviewItem(editReview.getText().toString(), String.valueOf(rating));     //작성한 리뷰와 별점이 파이어베이스에 올라감
-                    FirebaseDatabase.getInstance().getReference().child("Review").child(menu).child(formatDate).setValue(a); //formatData는 작성한 시간
-                    finish();
-                    return true;
+                    if (isimage) {
+                        ReviewItem a = new ReviewItem(editReview.getText().toString(), String.valueOf(rating), String.valueOf(downloadUrl), isimage);     //작성한 리뷰와 별점이 파이어베이스에 올라감
+                        FirebaseDatabase.getInstance().getReference().child("Review").child(menu).child(formatDate).setValue(a); //formatData는 작성한 시간
+                        finish();
+                        return true;
+                    }
+
+                    else {
+                        ReviewItem a = new ReviewItem(editReview.getText().toString(), String.valueOf(rating), isimage);     //작성한 리뷰와 별점이 파이어베이스에 올라감
+                        FirebaseDatabase.getInstance().getReference().child("Review").child(menu).child(formatDate).setValue(a); //formatData는 작성한 시간
+                        finish();
+                        return true;
+                    }
+
                 }
         }
         return super.onOptionsItemSelected(item);
